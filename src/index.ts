84e39a6e4d6f7c4f7f590cf74cc9de3e30defad9
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-05-01 08:16:19 -0400

    [imp]:install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('probando')
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server = http.createServer(app);
+server.listen(3050, () => {
+  console.log("Server is running on http://localhost:3050");
+});
