58236632f7711894abee2568aaabf16a87577e7a
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-05-01 08:26:48 -0400

    [imp]:mongodb (mongoose)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
@@ -14,4 +17,12 @@ app.use(compression());
 const server = http.createServer(app);
 server.listen(3050, () => {
   console.log("Server is running on http://localhost:3050");
+});
+
+const MONGO_URL = process.env.MONGO_URL;//env variable
+console.log(MONGO_URL);
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error) => {
+  console.log(error);
 });
